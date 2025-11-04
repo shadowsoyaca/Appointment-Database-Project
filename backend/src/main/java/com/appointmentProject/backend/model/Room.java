0dcb73d93bbc1076747e14b7f3fdfd4f78ecc578
shadowsoyaca <matthewkiy@gmail.com>
2025-11-04 10:32:43 -0600

    Room Model
    - Added a Room model
    - Compiler was Auto-Updated

---
+++ backend/src/main/java/com/appointmentProject/backend/model/Room.java
@@ -0,0 +1,39 @@
+package com.appointmentProject.backend.model;
+/******************************************************************************
+ * Room.java
+ *
+ *     Represents a Room entity from the database in object
+ *     format for transferring from the database to the frontend.
+ *      - "room_number": the number that is assigned to the room (3-4 char length)
+ *      - "floor_number": the floor the room is on.
+ *
+ *
+ * @author Matthew Kiyono
+ * @version 1.0
+ * @since 11/4/2025
+ ********************************************************************************/
+public class Room {
+
+    //variables
+    private int room_number;
+    private int floor_number;
+
+    //constructor
+    public Room(int room_number, int floor_number) {
+        this.room_number = room_number;
+        this.floor_number = floor_number;
+    }
+
+    //getter methods
+    public int getRoom_number() {return room_number;}
+    public int getFloor_number() {return floor_number;}
+
+    //toString method
+    @Override
+    public String toString() {
+        return "Room:" +
+                "\nNumber: " +  room_number +
+                "\nFloor: " + floor_number;
+    }
+
+}
